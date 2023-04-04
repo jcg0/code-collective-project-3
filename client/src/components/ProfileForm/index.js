7ae9f784edc299041ba25b980b60a8979407ed92
jcg0 <jg.awsprod@gmail.com>
2023-04-04 17:39:34 -0400

    added some more styles for better user experience

--- client/src/components/ProfileForm/index.js
+++ client/src/components/ProfileForm/index.js
@@ -70,116 +70,189 @@ const ProfileForm = ({ profiles, title, loading }) => {
 
   return (
     <div className="">
-      {clicked ? (
+      {Auth.loggedIn() ? (
         <>
-          <form
-            className="flex flex-col items-center text-start bg-neutral"
-            onSubmit={handleFormSubmit}
-          >
-            <label
-              className="text-4xl text-stone-200 font-extrabold"
-              htmlFor="profile_bio"
-            >
-              Bio
-            </label>
-            <textarea
-              className="textarea bg-secondary textarea-accent w-full max-w-xs"
-              name="bio"
-              id="profile_bio"
-              cols="30"
-              rows="7"
-              type="text"
-              value={profileForm.bio}
-              onChange={handleInputChange}
-              placeholder="Add a bio"
-            ></textarea>
-            <label
-              className="text-4xl text-stone-200 font-extrabold"
-              htmlFor=""
-            >
-              Skills
-            </label>
-            <input
-              className="input-md border-accent bg-secondary rounded input-bordered input-accent w-full max-w-xs"
-              type="text"
-              name="skills"
-              value={profileForm.skills}
-              onChange={handleInputChange}
-              placeholder="Skills"
-            />
-            <button
-              id="int-skl-btn"
-              className="m-4 btn glass bg-accent text-slate-950"
-              type="button"
-            >
-              add skill
-            </button>
-            <label
-              className="text-4xl text-stone-200 font-extrabold"
-              htmlFor=""
-            >
-              Interests
-            </label>
-            <input
-              className="input-md bg-secondary rounded input-bordered input-accent w-full max-w-xs"
-              type="text"
-              name="interests"
-              value={profileForm.interests}
-              onChange={handleInputChange}
-              placeholder="Interests"
-            />
-            <button
-              id="int-skl-btn"
-              className="m-4 btn glass bg-accent text-slate-950"
-              type="button"
-            >
-              Add Interest
-            </button>
-            <label
-              className="text-4xl text-stone-200 font-extrabold"
-              htmlFor=""
-            >
-              Website
-            </label>
-            <input
-              className="input-md border-accent bg-secondary rounded input-bordered input-accent w-full max-w-xs"
-              type="text"
-              name="websites"
-              value={profileForm.websites}
-              onChange={handleInputChange}
-              placeholder="website"
-            />
-            <label
-              className="text-4xl text-stone-200 font-extrabold"
-              htmlFor=""
-            >
-              Location
-            </label>
-            <input
-              className="input-md border-accent bg-secondary rounded input-bordered input-accent w-full max-w-xs"
-              type="text"
-              name="location"
-              value={profileForm.location}
-              onChange={handleInputChange}
-              placeholder="Location"
-            />
-            <button
-              id="prof-btn"
-              className="m-4 btn glass bg-accent text-slate-950"
-              type="submit"
-            >
-              save
-            </button>
-          </form>
+          {clicked ? (
+            <>
+              <form
+                className="flex flex-col items-center text-start bg-secondary"
+                onSubmit={handleFormSubmit}
+              >
+                <label
+                  className="text-4xl text-stone-200 font-extrabold"
+                  htmlFor="profile_bio"
+                >
+                  Bio
+                </label>
+                <textarea
+                  className="textarea bg-secondary-focus textarea-accent w-full max-w-xs"
+                  name="bio"
+                  id="profile_bio"
+                  cols="30"
+                  rows="7"
+                  type="text"
+                  value={profileForm.bio}
+                  onChange={handleInputChange}
+                  placeholder="Add a bio"
+                ></textarea>
+                <label
+                  className="text-4xl text-stone-200 font-extrabold"
+                  htmlFor=""
+                >
+                  Skills
+                </label>
+                <input
+                  className="input-md bg-secondary-focus bg-primary rounded input-bordered input-accent w-full max-w-xs"
+                  type="text"
+                  name="skills"
+                  value={profileForm.skills}
+                  onChange={handleInputChange}
+                  placeholder="Skills"
+                />
+                <button
+                  id="int-skl-btn"
+                  className="m-4 btn glass bg-accent text-slate-950"
+                  type="button"
+                  disabled
+                >
+                  add skill
+                </button>
+                <label
+                  className="text-4xl text-stone-200 font-extrabold"
+                  htmlFor=""
+                >
+                  Interests
+                </label>
+                <input
+                  className="input-md bg-secondary-focus rounded input-bordered input-accent w-full max-w-xs"
+                  type="text"
+                  name="interests"
+                  value={profileForm.interests}
+                  onChange={handleInputChange}
+                  placeholder="Interests"
+                />
+                <button
+                  id="int-skl-btn"
+                  className="m-4 btn glass bg-accent text-slate-950"
+                  type="button"
+                  disabled
+                >
+                  Add Interest
+                </button>
+                <label
+                  className="text-4xl text-stone-200 font-extrabold"
+                  htmlFor=""
+                >
+                  Website
+                </label>
+                <input
+                  className="input-md border-accent bg-secondary-focus rounded input-bordered input-accent w-full max-w-xs"
+                  type="text"
+                  name="websites"
+                  value={profileForm.websites}
+                  onChange={handleInputChange}
+                  placeholder="website"
+                />
+                <label
+                  className="text-4xl text-stone-200 font-extrabold"
+                  htmlFor=""
+                >
+                  Location
+                </label>
+                <input
+                  className="input-md border-accent bg-secondary-focus rounded input-bordered input-accent w-80 md:w-3/6 lg:w-1/2 xl:w-1/2"
+                  type="text"
+                  name="location"
+                  value={profileForm.location}
+                  onChange={handleInputChange}
+                  placeholder="Location"
+                />
+                <button
+                  id="prof-btn"
+                  className="m-4 btn glass bg-accent text-slate-950"
+                  type="submit"
+                >
+                  save
+                </button>
+              </form>
+            </>
+          ) : (
+            <>
+              <div className="pt-1 bg-secondary flex flex-col items-center">
+                <div className="card text-center card-bordered bg-secondary-focus border-accent m-5 p-5 shadow-2xl shadow-black w-80 md:w-3/6 lg:w-1/2 xl:w-1/3 h-fit">
+                  <span className="text-4xl text-stone-200 font-extrabold">
+                    Bio
+                  </span>
+                  <p className="text-stone-200">{profileForm.bio}</p>
+                </div>
+                <div className="card text-center card-bordered bg-secondary-focus border-accent m-5 p-5 shadow-2xl shadow-black w-80 md:w-3/6 lg:w-1/2 xl:w-1/3 h-fit">
+                  <p className="text-4xl text-stone-200 font-extrabold">
+                    Skills
+                  </p>
+                  <ul>
+                    {/* {profileForm.skills &&
+                      profileForm.skills.map((skills, index) => (
+                        <li key={skills.name}>{skills}</li>
+                      ))} */}
+                    <li className="text-stone-200">{profileForm.skills}</li>
+                  </ul>
+                </div>
+                <div className="card text-center card-bordered bg-secondary-focus border-accent m-5 p-5 shadow-2xl shadow-black w-80 md:w-3/6 lg:w-1/2 xl:w-1/3 h-fit">
+                  <p className="text-4xl text-stone-200 font-extrabold">
+                    Interests
+                  </p>
+                  <ul>
+                    {/* {profileForm.interests &&
+                profileForm.interests.map((interests, index) => (
+                  <li key={interests.name}>{interests.interests[index]}</li>
+                ))} */}
+                    <li className="text-stone-200">{profileForm.interests}</li>
+                  </ul>
+                </div>
+                <div className="card text-center card-bordered bg-secondary-focus border-accent m-5 p-5 shadow-2xl shadow-black w-80 md:w-3/6 lg:w-1/2 xl:w-1/3 h-fit">
+                  <p className="text-4xl text-stone-200 font-extrabold">
+                    Websites
+                  </p>
+                  <ul>
+                    {/* {profileForm.websites &&
+                profileForm.websites.map((websites, index) => (
+                  <li key={websites.name}>{websites.websites}</li>
+                ))} */}
+                    <li>
+                      <a className="text-stone-200" href={profileForm.websites}>
+                        {profileForm.websites}
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+                <div className="card text-center card-bordered bg-secondary-focus border-accent m-5 p-5 shadow-2xl shadow-black w-80 md:w-3/6 lg:w-1/2 xl:w-1/3 h-fit">
+                  <p className="text-4xl text-stone-200 font-extrabold">
+                    Location
+                  </p>
+                  <p className="text-stone-200">{profileForm.location}</p>
+                </div>
+                <div className="flex flex-col items-center pb-6">
+                  <button
+                    className="btn glass btn-wide bg-accent text-slate-950"
+                    onClick={handleClick}
+                  >
+                    edit profile
+                  </button>
+                </div>
+              </div>
+            </>
+          )}
         </>
       ) : (
         <>
-          <div className="pt-1 bg-focus">
-            <div className="card card-bordered bg-secondary border-accent m-5 p-5 shadow-2xl shadow-black">
-              <p className="text-4xl text-stone-200 font-extrabold">Bio</p>
+          <div className="pt-1 bg-secondary flex flex-col items-center">
+            <div className="card text-center card-bordered bg-secondary-focus border-accent m-5 p-5 shadow-2xl shadow-black w-80 md:w-3/6 lg:w-1/2 xl:w-1/3 h-fit">
+              <span className="text-4xl text-stone-200 font-extrabold">
+                Bio
+              </span>
               <p className="text-stone-200">{profileForm.bio}</p>
             </div>
-            <div className="card card-bordered bg-secondary border-accent m-5 p-5 shadow-2xl shadow-black">
+            <div className="card text-center card-bordered bg-secondary-focus border-accent m-5 p-5 shadow-2xl shadow-black w-80 md:w-3/6 lg:w-1/2 xl:w-1/3 h-fit">
               <p className="text-4xl text-stone-200 font-extrabold">Skills</p>
               <ul>
                 {/* {profileForm.skills &&
@@ -189,7 +262,7 @@ const ProfileForm = ({ profiles, title, loading }) => {
                 <li className="text-stone-200">{profileForm.skills}</li>
               </ul>
             </div>
-            <div className="card card-bordered bg-secondary border-accent m-5 p-5 shadow-2xl shadow-black">
+            <div className="card text-center card-bordered bg-secondary-focus border-accent m-5 p-5 shadow-2xl shadow-black w-80 md:w-3/6 lg:w-1/2 xl:w-1/3 h-fit">
               <p className="text-4xl text-stone-200 font-extrabold">
                 Interests
               </p>
@@ -201,7 +274,7 @@ const ProfileForm = ({ profiles, title, loading }) => {
                 <li className="text-stone-200">{profileForm.interests}</li>
               </ul>
             </div>
-            <div className="card card-bordered bg-secondary border-accent m-5 p-5 shadow-2xl shadow-black">
+            <div className="card text-center card-bordered bg-secondary-focus border-accent m-5 p-5 shadow-2xl shadow-black w-80 md:w-3/6 lg:w-1/2 xl:w-1/3 h-fit">
               <p className="text-4xl text-stone-200 font-extrabold">Websites</p>
               <ul>
                 {/* {profileForm.websites &&
@@ -215,17 +288,9 @@ const ProfileForm = ({ profiles, title, loading }) => {
                 </li>
               </ul>
             </div>
-            <div className="card card-bordered bg-secondary border-accent m-5 p-5 shadow-2xl shadow-black">
+            <div className="card text-center card-bordered bg-secondary-focus border-accent m-5 p-5 shadow-2xl shadow-black w-80 md:w-3/6 lg:w-1/2 xl:w-1/3 h-fit">
               <p className="text-4xl text-stone-200 font-extrabold">Location</p>
               <p className="text-stone-200">{profileForm.location}</p>
-            </div>
-            <div className="flex flex-col items-center pb-6">
-              <button
-                className="btn glass btn-wide bg-accent text-slate-950"
-                onClick={handleClick}
-              >
-                edit profile
-              </button>
             </div>
           </div>
         </>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,30 +1,35 @@
 import React from "react";
 import { useQuery } from "@apollo/client";
 import { QUERY_USER_LIST } from "../utils/queries";
-import PostForm from '../components/PostForm'; 
-import PostList from '../components/PostList'; 
+import PostForm from "../components/PostForm";
+import PostList from "../components/PostList";
 
 const Home = () => {
-  const { loading, data } = useQuery(QUERY_USER_LIST); 
+  const { loading, data } = useQuery(QUERY_USER_LIST);
 
   const users = data?.users || [];
-  console.log(users); 
+  console.log(users);
 
   return (
     <main className="home-container bg-secondary">
-      <div className="card justify-center object-center">
+      <div className="card justify-start object-center ">
         <PostForm />
       </div>
-        {loading ? 
-        (<div>Loading...</div>
-        ) : (
-          <>
-          {users && 
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          {users &&
             users.map((user) => (
-              <PostList posts={user.posts}  firstName={user.firstName} lastName={user.lastName} username={user.username}/>
+              <PostList
+                posts={user.posts}
+                firstName={user.firstName}
+                lastName={user.lastName}
+                username={user.username}
+              />
             ))}
-          </>
-        )}
+        </>
+      )}
     </main>
   );
 };
